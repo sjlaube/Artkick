140b5bf6c283eaa6a2aee42420bbcf878116ec70
sjlaube <sjlaube@gmail.com>
2013-10-24 17:40:31 -0700

    fix google analytics to fire on refresh

--- SmartDevice/Test/WebContent/app.js
+++ SmartDevice/Test/WebContent/app.js
@@ -2212,7 +2212,13 @@ function refreshView() {
 	if(!window.email){
 		return;
 	}
-	
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44460273-1', 'artkick.net'); <!-- change this to -1 for production -->
+  ga('send', 'pageview');
 	window.justRefresh = true;
 	window.switchView = true;
 	var currView = dijit.registry.byId("Intro0");
